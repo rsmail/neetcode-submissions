041ef2713712d0e545ea6f3aae86a13bf30091a2
rsmail <123703783+rsmail@users.noreply.github.com>
2026-06-25 11:50:27 -0700

    Add: buy-and-sell-crypto - submission-23

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-23.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        maxP = 0
+        for r in range(len(prices)):
+            prof = (prices[r] - prices[l])
+            maxP = max(maxP, prof)
+
+            if prof < 0:
+                l = r
+        return maxP
+
+
+        
