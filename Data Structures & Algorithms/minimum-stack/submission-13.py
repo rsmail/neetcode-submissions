30e03578f016e312d3ff4d04d334d1c82abe9962
rsmail <123703783+rsmail@users.noreply.github.com>
2026-06-27 16:54:21 -0700

    Add: minimum-stack - submission-13

---
+++ Data Structures & Algorithms/minimum-stack/submission-13.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        stack = []
+        minStack = []
+        self.stack = stack
+        self.minStack = minStack
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            val = min(val, self.minStack[-1])
+        self.minStack.append(val)
+
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minStack[-1] 
+
+        
