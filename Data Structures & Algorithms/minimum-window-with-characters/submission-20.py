2201e62c2ffe027ce99484809eaf32db0297e08c
rsmail <123703783+rsmail@users.noreply.github.com>
2026-07-02 16:38:33 -0700

    Add: minimum-window-with-characters - submission-20

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-20.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s): return ""
+
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = countT.get(c, 0) + 1
+        
+        l = 0
+        have, need = 0, len(countT)
+        res = [-1, -1]
+        resLen = sys.maxsize
+
+        for r in range(len(s)):
+            c = s[r]
+
+            window[c] = window.get(c, 0) + 1
+
+            if c in countT and countT[c] == window[c]:
+                have += 1
+            while have == need:
+                if resLen > (r - l + 1):
+                    resLen = (r - l + 1)                    
+                    res = [l,r]
+                if s[l] in countT and window[s[l]] - 1 < countT[s[l]]:
+                    have -= 1
+                window[s[l]] -= 1
+                l += 1
+        l, r = res
+        if resLen != sys.maxsize: 
+            return s[l:r+1] 
+        else: 
+            return ""
