e7114e099be223fd532982eab03223675f58cd67
rsmail <123703783+rsmail@users.noreply.github.com>
2026-07-05 23:23:01 -0700

    Add: search-2d-matrix - submission-11

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-11.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #two binary searches here. first the rows, then the cols
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            m = (top + bot) // 2
+            if matrix[m][-1] < target:
+                top = m + 1
+            elif matrix[m][0] > target:
+                bot = m - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        m = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[m][mid] < target:
+                l = mid + 1
+            elif matrix[m][mid] > target:
+                r = mid -1
+            else:
+                return True
+        return False
+
+        
